fe7702c5f03b71fc73b4344ddb87220bc62e00d4
Bruno Vasconcelos <brunohgv2@gmail.com>
2019-08-15 23:42:51 -0300

    Init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fluttagram',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fluttagram'),
+        ),
+      ),
+    );
+  }
+}
